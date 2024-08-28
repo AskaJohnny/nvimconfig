8709b431ba7027316b5d8655ec622b1ff68d9576
张强 <johnny.zhang@weiheng-tech.com>
2024-08-28 12:14:47 +0800

    very good for dapui

--- lua/plugins/plugin-nvim-dap-ui.lua
+++ lua/plugins/plugin-nvim-dap-ui.lua
@@ -1,204 +1,109 @@
 return {
-    {
-        "rcarriga/nvim-dap-ui",
-        dependencies = {
-            "nvim-neotest/nvim-nio",
-        },
-        config = function()
-            local user_config = {
-                controls = {
-                    element = "repl",
-                    enabled = false,
-                    icons = {
-                        disconnect = "",
-                        pause = "",
-                        play = "",
-                        run_last = "",
-                        step_back = "",
-                        step_into = "",
-                        step_out = "",
-                        step_over = "",
-                        terminate = "",
-                    },
-                },
-                element_mappings = {},
-                expand_lines = true,
-                floating = {
-                    border = "single",
-                    mappings = {
-                        close = { "q", "<Esc>" },
-                    },
-                },
-                force_buffers = true,
-                icons = {
-                    collapsed = "",
-                    current_frame = "",
-                    expanded = "",
-                },
-                layouts = {
-                    {
-                        elements = {
-                            { id = "scopes",      size = 0.25 },
-                            { id = "breakpoints", size = 0.25 },
-                            {
-                                id = "stacks",
-                                size = 0.25,
-                            },
-                            {
-                                id = "watches",
-                                size = 0.25,
-                            },
-                        },
-                        position = "left",
-                        size = 40,
-                    },
-                    {
-                        elements = {
-                            -- {
-                            --     id = "repl",
-                            --     size = 0.5,
-                            -- },
-                            {
-                                id = "console",
-                                size = 1,
-                            },
-                        },
-                        position = "bottom",
-                        size = 10,
-                    },
-                },
-                mappings = {
-                    edit = "e",
-                    expand = { "<CR>", "<2-LeftMouse>" },
-                    open = "o",
-                    remove = "d",
-                    repl = "r",
-                    toggle = "t",
-                },
-                render = {
-                    indent = 1,
-                    max_value_lines = 100,
-                },
-            }
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup({
+				icons = { expanded = "", collapsed = "", current_frame = "" },
+				layouts = {
+					{
+						-- You can change the order of elements in the sidebar
+						elements = {
+							-- Provide IDs as strings or tables with "id" and "size" keys
+							{
+								id = "scopes",
+								size = 0.25, -- Can be float or integer > 1
+							},
+							{ id = "breakpoints", size = 0.25 },
+							{ id = "stacks", size = 0.25 },
+							{ id = "watches", size = 0.25 },
+						},
+						size = 40,
+						position = "left",
+					},
+					{
+						elements = {
+							{ id = "console", size = 1 },
+						},
+						size = 10,
+						position = "bottom",
+					},
+				},
+			})
+			local M = { dapui_active = false }
 
-            -- 这里想实现 根据 lsp 是哪个 是java就开启console, 是 其他的就2个都开启
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(arg)
-                    local server_name = vim.lsp.get_client_by_id(arg.data.client_id).name
-                    if server_name == "jdtls" then
-                        -- table.insert(layouts, {
-                        -- 	elements = {
-                        -- 		{
-                        -- 			id = "console",
-                        -- 			size = 1,
-                        -- 		},
-                        -- 	},
-                        -- 	position = "bottom",
-                        -- 	size = 10,
-                        -- })
-                    elseif server_name == "kotlin_language_server" then
-                        -- table.insert(layouts, {
-                        --     elements = {
-                        --         {
-                        --             id = "repl",
-                        --             size = 1,
-                        --         },
-                        --     },
-                        --     position = "bottom",
-                        --     size = 10,
-                        -- })
-                    else
-                        -- table.insert(layouts, {
-                        --     elements = {
-                        --         {
-                        --             id = "repl",
-                        --             size = 0.5,
-                        --         },
-                        --         {
-                        --             id = "console",
-                        --             size = 0.5,
-                        --         },
-                        --     },
-                        --     position = "bottom",
-                        --     size = 10,
-                        -- })
-                    end
-                    -- user_config["layouts"] = layouts
-                    require("dapui").setup(user_config)
+			local function auto_close(bufnr)
+				local acid
+				acid = vim.api.nvim_create_autocmd({ "BufDelete", "BufHidden" }, {
+					buffer = bufnr,
+					callback = function()
+						print("bufd hiddle")
+						if M.dapui_active then
+							require("dapui").close()
+							M.dapui_active = false
+						else
+							dap.repl.close()
+						end
+						vim.api.nvim_del_autocmd(acid)
+					end,
+				})
+			end
 
-                    -- vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
-                    -- vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
-                    -- vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+			-- dap.defaults.fallback.terminal_win_cmd = "belowright 12new | set filetype=dap-terminal"
+			local dapui_console = dap.defaults.fallback.terminal_win_cmd
+			dap.defaults.fallback.terminal_win_cmd = function()
+				if M.dapui_active then
+					local bufnr = dapui_console()
+					auto_close(bufnr)
+					return bufnr
+				else
+					print("ter win cdm")
+					local cur_win = vim.api.nvim_get_current_win()
+					-- open terminal
+					vim.api.nvim_command("belowright 12new")
+					local bufnr = vim.api.nvim_get_current_buf()
+					vim.bo[bufnr].modifiable = false
+					vim.bo[bufnr].swapfile = false
+					vim.bo[bufnr].buftype = "nofile"
+					vim.bo[bufnr].filetype = "dap-terminal"
 
-                    local dap, dapui = require("dap"), require("dapui")
+					local win = vim.api.nvim_get_current_win()
+					vim.api.nvim_set_current_win(cur_win)
+					auto_close(bufnr)
+					return bufnr, win
+				end
+			end
 
-                    dap.listeners.before.attach.dapui_config = function()
-                        --dapui.toggle()
-                        dapui.open()
-                    end
-                    dap.listeners.before.launch.dapui_config = function()
-                        -- dapui.toggle()
-                        dapui.open()
-                    end
-                    dap.listeners.before.event_terminated.dapui_config = function()
-                        print("terminate")
-                        if dap.session().exited == true then
-                            print("terminate dap close")
-                            vim.api.nvim_del_augroup_by_name("DapUiAutoOpen")
-                            --dapui.close()
-                        end
-                    end
-                    dap.listeners.before.event_exited.dapui_config = function()
-                        print("exited")
-                        if dap.session().exited == true then
-                            print("exited dap close")
-                            --dapui.close()
-                        end
-                    end
-
-                    -- local debug_win = nil
-                    -- local debug_tab = nil
-                    -- local debug_tabnr = nil
-                    --
-                    -- local function open_in_tab()
-                    --     if debug_win and vim.api.nvim_win_is_valid(debug_win) then
-                    --         vim.api.nvim_set_current_win(debug_win)
-                    --         return
-                    --     end
-                    --
-                    --     vim.cmd("tabedit %")
-                    --     debug_win = vim.fn.win_getid()
-                    --     debug_tab = vim.api.nvim_win_get_tabpage(debug_win)
-                    --     debug_tabnr = vim.api.nvim_tabpage_get_number(debug_tab)
-                    --
-                    --     print('hhhh')
-                    --     dapui.open()
-                    -- end
-                    --
-                    -- local function close_tab()
-                    --     dapui.close()
-                    --
-                    --     if debug_tab and vim.api.nvim_tabpage_is_valid(debug_tab) then
-                    --         vim.api.nvim_exec("tabclose " .. debug_tabnr, false)
-                    --     end
-                    --
-                    --     debug_win = nil
-                    --     debug_tab = nil
-                    --     debug_tabnr = nil
-                    -- end
-                    --
-                    -- -- Attach DAP UI to DAP events
-                    -- dap.listeners.after.event_initialized["dapui_config"] = function()
-                    --     open_in_tab()
-                    -- end
-                    -- dap.listeners.before.event_terminated["dapui_config"] = function()
-                    --     close_tab()
-                    -- end
-                    -- dap.listeners.before.event_exited["dapui_config"] = function()
-                    --     close_tab()
-                    -- end
-                end,
-            })
-        end,
-    },
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				-- dapui.open()
+				if not M.dapui_active then
+					-- dap.repl.open()
+				end
+				require("dapui").open()
+			end
+			-- dap.listeners.before.event_terminated["dapui_config"] = function()
+			--   dapui.close()
+			-- end
+			-- dap.listeners.before.event_exited["dapui_config"] = function()
+			--   dapui.close()
+			-- end
+			-- nvim-dap
+			vim.api.nvim_create_user_command("DapUIOpen", function()
+				M.dapui_active = true
+				require("dapui").open()
+			end, {})
+			vim.api.nvim_create_user_command("DapUIClose", function()
+				M.dapui_active = false
+				require("dapui").close()
+			end, {})
+			vim.api.nvim_create_user_command("DapUIToggle", function()
+				M.dapui_active = not M.dapui_active
+				require("dapui").toggle()
+			end, {})
+		end,
+	},
 }
--- lua/plugins/plugin-nvim-tree.lua
+++ lua/plugins/plugin-nvim-tree.lua
@@ -172,7 +172,7 @@ return {
                     },
                 },
                 open_file = {
-                    quit_on_open = true,
+                    quit_on_open = false,
                     eject = true,
                     resize_window = true,
                     window_picker = {
